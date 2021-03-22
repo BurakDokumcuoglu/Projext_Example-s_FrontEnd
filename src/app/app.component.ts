cc6add8006e4749570b371fde484de5ce15bb0b6
burakdokumcuoglu3@gmail.com <burakdokumcuoglu3@gmail.com>
2021-03-22 23:22:59 +0300

    22 Mart 2021

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core';  //component kullanabilmek için gerekli olan satır
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+@Component({  //AppComponent class'ının component olduğu belirtiliyor
+  selector: 'app-root', //index.html'de app-root taginin kullanılacağı belirtiliyor
+  templateUrl: './app.component.html', //component bu html'in datasını yönetecek 
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'northwind';
+  user: string = "Burak Dökümcüoğlu";
+ 
 }
---
+++ src/app/models/product.ts
@@ -0,0 +1,7 @@
+export interface Product{  //js ve ts'de interface yaratma şekli
+    productId:number;
+    categoryId:number;
+    productName:string;
+    unitsInStock:number;     
+    unitPrice:number;
+}
